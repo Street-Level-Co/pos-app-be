dd78844e09c90e619662c418b0f4c38900c6d3d6
Isuru Bandara <icbandara@controlunion.com>
2026-08-28 19:09:42 +0530

    add user-organization

--- src/main/java/com/example/demo/model/ClientOrganization.java
+++ src/main/java/com/example/demo/model/ClientOrganization.java
@@ -1,11 +1,7 @@
 package com.example.demo.model;
 
-import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
